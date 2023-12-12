3f0a71c2e0ff2e946d97c1ea7cf334f9c351a2fe
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2023-12-11 17:02:49 -0800

    Create math operators functions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//Create math operators:
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
