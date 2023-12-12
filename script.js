b200432cbda25337ac96b106ee1e7ecdbda66c95
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2023-12-11 17:16:49 -0800

    Add an operate function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function division(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+//Operate function where it will call one of the above 
+//function to do the job when users input the operators and 2 numbers
+function operate(numbA, numbB, operator) {
+    switch (operator) {
+        case '+':
+            console.log(add(numbA, numbB));
+            break;
+        case '-':
+            console.log(substract(numbA, numbB));
+            break;
+        case 'x':
+            console.log(multiply(numbA, numbB));
+            break;
+        case '/':
+            console.log(division(numbA, numbB));
+            break;
+    }
+}
